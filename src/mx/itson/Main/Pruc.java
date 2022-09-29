43b6feeaa80d9d44e82e10be760cbd628854e6e9
chiqu <chiqu@DESKTOP-9Q5COMS>
2022-09-28 19:49:06 -0700

    primera version

---
+++ src/mx/itson/Main/Pruc.java
@@ -0,0 +1,21 @@
+
+package mx.itson.Main;
+
+
+public class Pruc {
+    
+    public Pruc(){
+        
+    }
+    
+    Integer dia=0, año=0, mes=0;
+            
+    public void getFecha(String a, String b, String c){
+        
+        dia=Integer.parseInt(a);
+        mes=Integer.parseInt(b);
+        año=Integer.parseInt(c);
+        
+    }        
+    
+}
